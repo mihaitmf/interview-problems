91c76ecd43b589b981c909e546b88c8d461d1d58
mihaitmf <mihai.tmf@gmail.com>
2024-02-28 06:41:01 +0200

    Add some solutions to algorithm problems

--- algorithms/highestProduct/run.php
+++ algorithms/highestProduct/run.php
@@ -10,6 +10,61 @@
  * by calculating: 3 * 5 * 3 = 45
  *
  * For example, given: [3, 1, 0, 5, 3, 2, -1, -5, -2]
- * your function would return: 45
+ * your function would return: 50
  * by calculating: 5 * -5 * -2 = 50
  */
+
+$input = [3, 1, 0, 5, 3, 2, -1, -5, -2];
+$expectedResult = 50;
+$result = highestProductOf3($input);
+
+if ($result === $expectedResult) {
+    echo "SUCCESS\n";
+} else {
+    echo "FAIL\n";
+}
+
+function highestProductOf3($input)
+{
+    $highest = max($input[0], $input[1]);
+    $lowest  = min($input[0], $input[1]);
+
+    $highestProductOf2 = $input[0] * $input[1];
+    $lowestProductOf2  = $input[0] * $input[1];
+
+    $highestProductOf3 = $input[0] * $input[1] * $input[2];
+
+    // walk through items, starting at index 2
+    for ($i = 2; $i < count($input); $i++) {
+        $current = $input[$i];
+
+        // do we have a new highest product of 3?
+        $highestProductOf3 = max(
+            $highestProductOf3,
+            $current * $highestProductOf2,
+            $current * $lowestProductOf2
+        );
+
+        // do we have a new highest product of two?
+        $highestProductOf2 = max(
+            $highestProductOf2,
+            $current * $highest,
+            $current * $lowest
+        );
+
+        // do we have a new lowest product of two?
+        $lowestProductOf2 = min(
+            $lowestProductOf2,
+            $current * $highest,
+            $current * $lowest
+        );
+
+        // do we have a new highest?
+        $highest = max($highest, $current);
+
+        // do we have a new lowest?
+        $lowest = min($lowest, $current);
+    }
+
+    return $highestProductOf3;
+}
--- algorithms/productOfOtherNumbers/run.php
+++ algorithms/productOfOtherNumbers/run.php
@@ -10,3 +10,36 @@
  * your function would return: [84, 12, 28, 21]
  * by calculating: [7 * 3 * 4, 1 * 3 * 4, 1 * 7 * 4, 1 * 7 * 3]
  */
+
+$input = [1, 7, 3, 4];
+$expectedResult = [84, 12, 28, 21];
+$result = getProductsOfAllIntsExceptAtIndex($input);
+
+if ($result === $expectedResult) {
+    echo "SUCCESS\n";
+} else {
+    echo "FAIL\n";
+}
+
+function getProductsOfAllIntsExceptAtIndex($input) {
+    $n = count($input);
+
+    $prodsBeforeIndex = [];
+    $prodsAfterIndex = [];
+    $productBefore = 1;
+    $productAfter = 1;
+    for ($i = 0; $i < $n; $i++) {
+        $prodsBeforeIndex[$i] = $productBefore;
+        $productBefore *= $input[$i];
+
+        $prodsAfterIndex[$n - 1 - $i] = $productAfter;
+        $productAfter *= $input[$n - 1 - $i];
+    }
+
+    $prods = [];
+    for ($i = 0; $i < $n; $i++) {
+        $prods[$i] = $prodsBeforeIndex[$i] * $prodsAfterIndex[$i];
+    }
+
+    return $prods;
+}
